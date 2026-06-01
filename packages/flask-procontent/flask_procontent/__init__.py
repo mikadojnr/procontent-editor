190d0c32926232e064e3db126f70a03b26f0edc3
Edidiong Udoidiok <101645624+mikadojnr@users.noreply.github.com>
2026-06-01 12:31:11 +0100

    feat: Flask package init

---
+++ packages/flask-procontent/flask_procontent/__init__.py
@@ -0,0 +1,8 @@
+"""
+ProContent Flask Integration
+"""
+
+from flask_procontent import Editor, upload_routes, render_routes
+
+__version__ = '1.0.0'
+__all__ = ['Editor', 'upload_routes', 'render_routes']
